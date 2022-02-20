c570f395c4040dc0f55c02cb9c94ddc2bbe21783
louie camacho dxa <louiecamacho@gmail.com>
2022-02-20 13:31:30 -0600

    separate components

---
+++ my-app/src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<small>&copy; Buttonbag development. All rights reserved.</small>
+		</footer>
+	);
+}
+
+
+export default Footer;
