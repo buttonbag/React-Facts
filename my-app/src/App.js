68f4f737affadc45135e19127076c51407e918ff
louie camacho dxa <louiecamacho@gmail.com>
2022-02-20 13:31:14 -0600

    new scss

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-
-function Header() {
-	return (
-		<header>
-			<nav>
-				<img src={logo} alt="" width="40px" />
-				<ul className="nav-items">
-					<li>Pricing</li>
-					<li>About</li>
-					<li>Contact</li>
-				</ul>
-			</nav>
-		</header>
-	);
-}
-function MainContent() {
-	return (
-		<div>
-			<h1>Fun facts about React</h1>
-			<ul>
-				<li>released in 2013</li>
-				<li>created by jordan walke</li>
-				<li>over 100k stars on Github</li>
-				<li>maintained by Facebook overlords</li>
-				<li>Powers thousands of enterprise apps, including mobile apps</li>
-			</ul>
-		</div>
-	);
-}
-function Footer() {
-	return (
-		<footer>
-			<small>&copy; Buttonbag development. All rights reserved.</small>
-		</footer>
-	);
-}
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
 	return (
